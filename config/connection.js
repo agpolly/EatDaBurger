dcc9f99d6dc752919030aaae4884d65ddacedc9a
Angela <agpolly@gmail.com>
2017-11-10 12:14:30 -0500

    heroku local web test

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,8 @@ if (process.env.JAWSDB_URL) {
 } else {
     connection = mysql.createConnection({
     host:'localhost',
-    user:'lw6c3q9za92y0y31',
-    password:'f6f2g0owx7ksv220',
+    user:'root',
+    password:'root',
     database:'burgers_db'
 });
 };
