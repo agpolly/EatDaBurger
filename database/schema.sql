7c6f26123ec1ece899955463513bbbb349942986
Angela <agpolly@gmail.com>
2017-11-10 11:14:05 -0500

    trying heroku again

--- database/schema.sql
+++ database/schema.sql
@@ -2,9 +2,10 @@ CREATE DATABASE IF NOT EXISTS burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-id int NOT NULL AUTO_INCREMENT,
+id int AUTO_INCREMENT NOT NULL,
 burger_name varchar(255) NOT NULL,
 devoured BOOL DEFAULT false,
+createdAt TIMESTAMP NOT NULL,
 PRIMARY KEY (id)
 
 );
