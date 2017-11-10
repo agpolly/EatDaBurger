55fff408745ced10d6a212704b8291652973a0c2
Angela <agpolly@gmail.com>
2017-11-10 12:09:13 -0500

    profile

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection;
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection("mysql://lw6c3q9za92y0y31:f6f2g0owx7ksv220@yhrz9vns005e0734.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/hi83mi64ajg4h8sk");
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
     host:'localhost',
