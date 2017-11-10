403b98d965f258e0f34418e069e977d3bb30e9a7
Angela <agpolly@gmail.com>
2017-11-10 11:49:48 -0500

    connection string

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,5 +1,3 @@
-USE burgers_db;
-
 INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza burger', FALSE);
